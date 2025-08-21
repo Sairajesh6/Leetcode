230132178937bd1d5f34bf608ffdc648549abbc7
Sairajesh6 <137755384+Sairajesh6@users.noreply.github.com>
2025-08-21 11:12:09 +0530

    Time: 32 ms (94.39%), Space: 18.6 MB (88.79%) - LeetHub

--- 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.py
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.py
@@ -1,28 +1,33 @@
 class Solution:
     def numSubmat(self, mat: List[List[int]]) -> int:
-        m=len(mat)
-        n=len(mat[0])
-        ans=0
-        width=[[0]*n for _ in range(m)]
-
+        m, n = len(mat), len(mat[0])
+        heights = [0] * n
+        res = 0
         for i in range(m):
             for j in range(n):
-                if mat[i][j] !=0:
-                    if j==0:
-                        width[i][j]=1
-                    else:
-                        width[i][j]=width[i][j-1]+1
+                if mat[i][j] == 1:
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
 
-        for i in range(m):
-            for j in range(n):
-                minwidth=width[i][j]
-                k=i
-                while k>=0 and minwidth >0:
-                    minwidth= min(minwidth,width[k][j])
-                    ans=ans+minwidth
-                    k=k-1
-        return ans 
-
-
-
+            res += self.countRow(heights)
         
+        return res
+    
+    def countRow(self, heights: List[int]) -> int:
+        stack = []
+        subs = [0] * len(heights)
+
+        for i, h in enumerate(heights):
+            while stack and heights[stack[-1]] >= h:
+                stack.pop()
+                
+            if stack:
+                left = stack[-1]
+                subs[i] = subs[left] + h * (i-left)
+            else:
+                subs[i] = h * (i+1)
+
+            stack.append(i)
+
+        return sum(subs)
