71c34b7b4f281ce60dd92b1f2dcfc6ed4f0644f8
Sairajesh6 <137755384+Sairajesh6@users.noreply.github.com>
2025-08-28 10:29:38 +0530

    Time: 15 ms (37.92%), Space: 18 MB (29.58%) - LeetHub

---
+++ LeetCode/Medium/3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        n = len(grid)
+
+        for i in range(n):
+            tmp = [grid[i+j][j] for j in range(n - i)]
+            tmp.sort(reverse = True)
+            for j in range(n-i):
+                grid[i+j][j] = tmp[j]
+        
+        for i in range(1, n):
+            tmp = [grid[j][i+j] for j in range(n - i)]
+            tmp.sort()
+            for j in range(n - i):
+                grid[j][i+j] = tmp[j]
+
+        return grid
