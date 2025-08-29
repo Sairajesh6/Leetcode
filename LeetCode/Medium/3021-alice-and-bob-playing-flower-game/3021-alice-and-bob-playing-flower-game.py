3b2a0c9ef1d83b809786a0ba44e9edd689d9c8f5
Sairajesh6 <137755384+Sairajesh6@users.noreply.github.com>
2025-08-29 10:00:29 +0530

    Time: 27 ms (6.25%), Space: 17.9 MB (15.97%) - LeetHub

--- LeetCode/Medium/3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
+++ LeetCode/Medium/3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -1,3 +1,10 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        return  (n * m) // 2
+        # return  (n * m) // 2
+        ans = 0
+        for i in range(1,n+1):
+            if i % 2:
+                ans += m // 2
+            else:
+                ans += (m+1) // 2
+        return ans
